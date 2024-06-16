84d4c20b2eea73bee77e17ffd80085402e234539
mamymananta <mamymananta@gmail.com>
2024-06-16 18:10:29 +0700

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$config = array(
-  	'url'		=> 'https://alibin.pages.dev/',
-	'parameter' => 'tunnel',
-	'template'  => 'temp.html',
-	'keywords'  => 'list.txt',
-	'sitemap'	=> 'sitemap.xml'
-);
-?>
