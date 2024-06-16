f7930c5637ef406d0eafe274675decb718b01b3e
mamymananta <mamymananta@gmail.com>
2024-06-16 18:02:41 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$config = array(
+  	'url'		=> 'https://alibin.pages.dev/',
+	'parameter' => 'tunnel',
+	'template'  => 'temp.html',
+	'keywords'  => 'list.txt',
+	'sitemap'	=> 'sitemap.xml'
+);
+?>
